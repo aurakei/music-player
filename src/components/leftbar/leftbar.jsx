561579c38680d0e4806daa455f788bc918f320a1
aurakei <aurakeith@gmail.com>
2024-01-19 22:36:59 +0300

    style

--- src/components/leftbar/leftbar.jsx
+++ src/components/leftbar/leftbar.jsx
@@ -1,8 +1,19 @@
-// LeftComponent.js
+// LeftBar.jsx
 import React from 'react';
 
-const LeftBar = () => {
-  return <div className="bg-gray-200 p-4 h-screen w-1/4">Left Component (Library)</div>;
+const LeftBar = ({ songs }) => {
+  return (
+    <div className="w-1/5 p-4 border border-gray-300 rounded-lg">
+      <h2 className="text-lg font-bold mb-4">Song List</h2>
+      <ul>
+        {songs?.map((song, index) => (
+          <li key={index} className="mb-2">
+            {song.title} - {song.primary_artist.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default LeftBar;
