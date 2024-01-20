d66bd7d502919077d9ddbd0196e81ac30a2d6a16
aurakei <aurakeith@gmail.com>
2024-01-20 22:53:09 +0300

    now playing section edits

--- src/components/cardList/nowPlayingCard.jsx
+++ src/components/cardList/nowPlayingCard.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const NowPlaying = ({ song }) => {
   return (
-    <div className="bg-gray-300 p-4">
+    <div className="w-[50%] p-7">
       <h2 className="text-lg font-semibold">Now Playing</h2>
       {song ? <p className="text-gray-600">{song.title}</p> : <p className="text-gray-600">No song selected</p>}
     </div>
